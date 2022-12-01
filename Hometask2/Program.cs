4608521560c5ccfa80762846ed13845c59ebb202
Victoria <vikochka90@mail.ru>
2022-12-02 02:03:47 +0300

    Вывести минимум и максимум

---
+++ Hometask2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write ("Введите число b = ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) 
+{Console.Write("max = " + a);
+Console.Write("min = " + b);
+
+}
+else 
+{Console.Write("max = " + b);
+Console.Write("min = " + a);
+}
