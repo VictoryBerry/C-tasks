5cf58badc8863c2a127a5dc45b48ed97dd2f9003
Victoria <vikochka90@mail.ru>
2022-12-02 14:45:41 +0300

    четное нечетное число

--- Hometask4/Program.cs
+++ Hometask4/Program.cs
@@ -1,16 +1,17 @@
-﻿Console.Write ("Введите число a = ");
+﻿Console.Write("Введите число a = ");
 int a = int.Parse(Console.ReadLine());
 
-Console.Write ("Введите число b = ");
+Console.Write("Введите число b = ");
 int b = int.Parse(Console.ReadLine());
 
-Console.Write ("Введите число c = ");
+Console.Write("Введите число c = ");
 int c = int.Parse(Console.ReadLine());
 
 int max = a;
 
-if (a > max)max = a; 
+if (a > max)max = a;
 if (b > max)max = b; 
 if (c > max)max = c; 
-{Console.Write(max);
+{  
+     Console.Write(max);
 }
---
+++ Hometask6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+
+
+
+if (a%2 == 0)
+{ Console.Write("ДА");
+}
+else 
+{ Console.Write("НЕТ");
+}
