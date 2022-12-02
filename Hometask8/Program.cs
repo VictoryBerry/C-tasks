4156697e634b84717723b399b8eb251f90f39dd4
Victoria <vikochka90@mail.ru>
2022-12-02 14:58:07 +0300

    Все четные числа до N

---
+++ Hometask8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine());
+
+int a = 1;
+
+while (a < N+1)
+{
+    if (a%2 == 0)
+    {
+        Console.Write(a);
+        a = a + 1;
+    }
+    else 
+    {
+        a = a + 1;
+    }
+    
+}
