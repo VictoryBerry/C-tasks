b85b879b1c1c857fbb1c3866a3619cc104b89a9f
Victoria <vikochka90@mail.ru>
2022-12-05 12:00:06 +0300

    correction

--- Hometask8/Program.cs
+++ Hometask8/Program.cs
@@ -1,18 +1,16 @@
 ﻿Console.Write("Введите число N = ");
 int N = int.Parse(Console.ReadLine());
 
-int a = 1;
+int a = 2;
 
-while (a < N+1)
+while (a <= N)
 {
     if (a%2 == 0)
     {
-        Console.Write(a);
-        a = a + 1;
-    }
-    else 
-    {
-        a = a + 1;
+        Console.Write(a + " ");
+        
     }
     
+ a = a + 1;   
+   
 }
