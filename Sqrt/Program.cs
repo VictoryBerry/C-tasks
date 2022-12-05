1a60c70cb372164538e2f7f98b16c386d75a64a5
Victoria <vikochka90@mail.ru>
2022-12-05 14:26:28 +0300

    является ли одно число квадратом другого

---
+++ Sqrt/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b = ");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b*b)
+{
+    Console.WriteLine("да одно число является квадратом другого");
+
+}
+
+else if (b == a*a)
+{
+    Console.WriteLine("да одно число является квадратом другого");
+}
+
+else
+{
+    Console.WriteLine("нет одно число не является квадратом другого");
+}
