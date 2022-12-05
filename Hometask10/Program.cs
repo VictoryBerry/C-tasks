be23731bb5a355b04ac932672ccb321a13c915ef
Victoria <vikochka90@mail.ru>
2022-12-05 16:07:51 +0300

    final correction

--- Hometask10/Program.cs
+++ Hometask10/Program.cs
@@ -4,4 +4,4 @@ int num = int.Parse(Console.ReadLine());
 int a2 = num / 10;
 int result = a2 % 10;
 
-Console.WriteLine($"число {num}  --> {result}");
+Console.WriteLine($"вторая цифра --> {result}");
