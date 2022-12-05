1f5c5a8063911ffdb08da4e56b8831fa0c34b52b
Victoria <vikochka90@mail.ru>
2022-12-05 16:03:50 +0300

    найти третью цифру числа

---
+++ Hometask13/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write ("Введите число num = ");
+int num = int.Parse(Console.ReadLine());
+int num2 = num;
+int count = 0;
+
+while (num2 > 0)
+{
+    num2 /= 10; 
+    count ++;
+}
+ if(count - 3 >= 0)
+ {
+   while (count - 3 > 0 ) 
+   {
+    num = num / 10;
+    count --;
+   }
+   Console.WriteLine($"Третья цифра числа --> {num % 10}");
+
+ }
+
+ else 
+ {
+    Console.WriteLine("Третьей цифры нет");
+ }
