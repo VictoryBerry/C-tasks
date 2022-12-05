a24aef881c3dec2b68a74fd94e907d789577f149
Victoria <vikochka90@mail.ru>
2022-12-05 13:50:36 +0300

    кратное число и остаток

---
+++ Kratnoe1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write ("Введите число b = ");
+int b = int.Parse(Console.ReadLine());
+int c = a / b;
+
+if (a % b == 0)
+{
+    Console.WriteLine($"число {b} кратно числу {a}");
+}
+
+if (a % b != 0)
+{
+    Console.WriteLine($"число {b} некратно числу {a} остаток {c}");
+}
