b016ec5ab73db3d10b768576d8019ff9bf1ee42c
Victoria <vikochka90@mail.ru>
2022-12-05 14:55:25 +0300

    определить выходной день

---
+++ Hometask15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите цифру, обозначающую день недели a = ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("Да, это выходной день");
+}
+
+else
+{
+    Console.WriteLine("нет, это не выходной");
+}
