5a88917c35d728ed6c1263abd50eb39accba6517
Victoria <vikochka90@mail.ru>
2022-12-05 14:47:16 +0300

    Вывести вторую цифру из трехзначного числа

---
+++ Hometask10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write ("Введите трехзначное число a = ");
+int num = int.Parse(Console.ReadLine());
+
+int a2 = num / 10;
+int result = a2 % 10;
+
+Console.WriteLine($"число {num}  --> {result}");
