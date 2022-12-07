ab4372e6dcad40a53bd3c7b398c1eb4e50014caa
Victoria <vikochka90@mail.ru>
2022-12-08 01:10:16 +0300

    нахождение квадрата числа последовательности

---
+++ task22Koordinati/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+
+Console.Write($" {N} -->");
+while (a <= N)
+{
+    int result = a * a;
+    Console.Write($" {result}");
+    a++;
+
+
+}
