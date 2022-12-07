5b37eac4dba24eae47f3374fd9f082b123d9026b
Victoria <vikochka90@mail.ru>
2022-12-08 01:42:01 +0300

    Таблица кубов

---
+++ Hometask23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+
+Console.Write($"Таблица кубов {N} -->");
+while (a <= N)
+{
+    int result = a * a * a;
+    Console.Write($" {result}");
+    a++;
+
+
+}
