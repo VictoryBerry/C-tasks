31d3cc0597b3eea0c6d93ed5e3189c6913150061
Victoria <vikochka90@mail.ru>
2022-12-08 00:11:49 +0300

    диапазон возможных координат

---
+++ task18Koordinati/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите номер четверти Q : ");
+int Q = int.Parse(Console.ReadLine());
+
+if (Q == 1)
+{
+    Console.WriteLine("X > 0 , Y > 0");
+}
+
+if (Q == 2)
+{
+    Console.WriteLine("X < 0 , Y > 0");
+}
+
+if (Q == 3)
+{
+    Console.WriteLine("X < 0 , Y < 0");
+}
+
+if (Q == 4)
+{
+    Console.WriteLine("X > 0 , Y < 0");
+}
