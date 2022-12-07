48ebbe23439eff7782528de3b8a4d3b7691bc486
Victoria <vikochka90@mail.ru>
2022-12-08 01:38:18 +0300

    определить является ли число полиндромом

---
+++ Hometask19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите пятизначное число N: ");
+int N = int.Parse(Console.ReadLine());
+
+
+if (N / 10000 == N % 10 && (N / 1000) % 10 == (N / 10) % 10)
+{
+    Console.WriteLine($" число {N} является полиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {N} не является полиндромом");
+}
