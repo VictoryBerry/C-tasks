ca243645ce336ce7db62f16398b1fc5aa2ff22f8
Victoria <vikochka90@mail.ru>
2022-12-08 00:54:20 +0300

    расстояние между точками

---
+++ task20Koordinati/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите координаты точки A: x1 ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты точки A: y1 ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты точки B: x2 ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты точки B: y2 ");
+int y2 = int.Parse(Console.ReadLine());
+
+
+
+double Length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+Console.Write($" расстояние между координатами {Length}");
