f08a77f23b1646bb34532b59a5ddc9e47c1db13e
Victoria <vikochka90@mail.ru>
2022-12-07 23:58:56 +0300

    определить четверть по заданным координатам

---
+++ task17Koordinati/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите координату X: ");
+int X = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координату Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Четверть 1");
+}
+
+if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Четверть 2");
+}
+
+if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Четверть 3");
+}
+
+if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Четверть 4");
+}
