c295108798e5b0fbea1e038e979a066be9842778
Victoria <vikochka90@mail.ru>
2022-12-11 23:37:37 +0300

    Замена одного символа на другой в тексте

---
+++ TextReplace/Program.cs
@@ -0,0 +1,18 @@
+﻿string text = "белеет парус одинокий"
+            + " в тумане моря голубом";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '-');
+Console.WriteLine(newText);
