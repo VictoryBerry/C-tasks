579399d62cf8bd87a4c3e5492680733d58ea9d43
Victoria <vikochka90@mail.ru>
2022-12-12 01:27:41 +0300

    метод вывести сумму

---
+++ task24method/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел = {GetSumNums(num)}");
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        Console.Write($"{number} ");
+        sum += number;
+        number--;
+
+    }
+    return sum;
+}
