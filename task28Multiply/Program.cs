44f6abfe3648e6cccf71f3aab469e92d0570ba71
Victoria <vikochka90@mail.ru>
2022-12-12 02:29:26 +0300

    Произведение ряда чисел

---
+++ task28Multiply/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($" = {Product(N)}");
+int Product(int number)
+{
+    int product = 1;
+    while (number > 0)
+    {
+        Console.Write($"{number} * ");
+        product = product * number;
+        number--;
+
+
+    }
+    return product;
+}
