b62e38aeb4aa4bc3bdb180412bbfe9f491b5b959
Victoria <vikochka90@mail.ru>
2022-12-12 02:18:43 +0300

    Вывести количество цифр в числе

---
+++ task26AmountOfNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Количество цифр в числе {num} --> {NumsAmount(num)}");
+Console.WriteLine($"Количество цифр в числе {num} --> {GetCount(num)}");
+int NumsAmount(int number)
+{
+    int count = 1;
+    while (number / 10 > 0)
+    {
+
+        count++;
+        number = number / 10;
+
+    }
+    return count;
+}
+//либо 
+
+int GetCount(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+
+
+        number = number / 10;
+        count++;
+
+    }
+    return count;
+}
