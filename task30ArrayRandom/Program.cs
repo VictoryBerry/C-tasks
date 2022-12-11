b45498caa9cf1e59d48fd7ee0cbe6c8b48a976c5
Victoria <vikochka90@mail.ru>
2022-12-12 02:49:08 +0300

    Массив с рандомными цифрами

---
+++ task30ArrayRandom/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array = new int[8];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int count = collection.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(collection[position]);
+        position++;
+    }
+}
+FillArray(array);
+PrintArray(array);
