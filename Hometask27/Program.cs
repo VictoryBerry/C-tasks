8d8321158c90ea116d518a7f84cec6ace7dfde2d
Victoria <vikochka90@mail.ru>
2022-12-12 03:39:36 +0300

    Сумма всех цифр числа

---
+++ Hometask27/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма всех цифр числа {num} = {SumNums(num)}");
+
+int SumNums(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
