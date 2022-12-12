cdcb8ec2e64c96aa8edd2d2cfda825e6c6876674
Victoria <vikochka90@mail.ru>
2022-12-12 03:30:15 +0300

    возвести число в степень

---
+++ Hometask25/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число A = ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B = ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine($" {A} в степени {B} = {Power(A, B)}");
+
+
+int Power(int number, int power)
+{
+    int result = 1;
+
+    while (power > 0)
+    {
+        result *= number;
+        power--;
+    }
+    return result;
+
+
+}
