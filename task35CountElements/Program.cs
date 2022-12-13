b740252590832613e2660b4394bf1997f8625d84
Victoria <vikochka90@mail.ru>
2022-12-14 01:14:54 +0300

    Количество определенных жлементов в массиве

---
+++ task35CountElements/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = GetArray(123, 0, 200);
+PrintArray(array);
+Console.WriteLine();
+int count = 0;
+
+foreach (int el in array)
+{
+    if (el > 10 && el < 99) count++;
+}
+Console.WriteLine($" Количество элементов значений от 10 до 99 --> {count}");
