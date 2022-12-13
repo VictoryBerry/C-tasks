fb873afaa0bcac619de3cfe2afca8f4e275e77e0
Victoria <vikochka90@mail.ru>
2022-12-14 00:41:31 +0300

    изменение элементов внутри массива

---
+++ task32ArrayChange/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = GetArray(12, -9, 9);
+PrintArray(array);
+Console.WriteLine();
+
+int[] ChangeArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++) array[i] *= -1;
+    return array;
+}
+
+PrintArray(ChangeArray(array));
+
