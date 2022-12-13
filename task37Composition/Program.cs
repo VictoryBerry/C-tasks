d9bae93cb4fcc9c574ca8349c29b180dd7f56d04
Victoria <vikochka90@mail.ru>
2022-12-14 02:11:31 +0300

    Произведение крайних чисел массива

---
+++ task37Composition/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = GetArray(11, 0, 10);
+PrintArray(array);
+Console.WriteLine("-->");
+
+
+int i = 0;
+int n = array.Length - 1;
+int Product;
+while (i < array.Length / 2)
+{
+    Product = array[i] * array[n];
+    i++;
+    n--;
+    Console.Write($" {Product} ");
+}
+
+
+
+
+
+
+
