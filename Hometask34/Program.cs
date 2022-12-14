622d44deea47eec27ad6a00f903f0298696582b2
Victoria <vikochka90@mail.ru>
2022-12-14 15:22:40 +0300

    Найти все четные числа массива

---
+++ Hometask34/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = GetArray(10, 100, 1000);
+PrintArray(array);
+Console.WriteLine();
+
+int count = 0;
+foreach (int el in array)
+{
+    if (el % 2 == 0) count++;
+}
+Console.WriteLine($" Количество четных элементов в массиве --> {count}");
