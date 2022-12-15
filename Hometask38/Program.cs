4033348927a364ac236af4f87434a9f7276e3062
Victoria <vikochka90@mail.ru>
2022-12-16 00:26:31 +0300

    Мин и макс массива разница значений

---
+++ Hometask38/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = GetArray(5, 1, 100);
+PrintArray(array);
+Console.WriteLine();
+
+
+
+void MaxEl(int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+        else if (array[i] < min) min = array[i];
+
+
+
+
+    }
+    int diff = max - min;
+
+    Console.Write($"Максимальное значение массива {max} , минимальное значение массива {min}  разница {diff}");
+
+}
+
+MaxEl(array);
+
+
