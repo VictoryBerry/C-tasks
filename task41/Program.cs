9c3395607012f3d613322457668ae1d464a02c7f
Victoria <vikochka90@mail.ru>
2022-12-21 01:27:59 +0300

    Двоичное число

---
+++ task41/Program.cs
@@ -0,0 +1,44 @@
+﻿// void ChangeToDouble(int i)
+// {
+//     int res = i % 2;
+//     while (i > 0)
+//     {
+//         if (res == 0)
+//         {
+//             Console.WriteLine("0");
+//             i = i / 2;
+
+//         }
+//         else
+//         {
+//             Console.WriteLine("1");
+//             i = i / 2;
+
+//         }
+
+//     }
+// }
+
+Console.Write("Введите число i: ");
+int i = int.Parse(Console.ReadLine());
+
+
+string numDouble = " ";
+
+
+while (i > 0)
+
+{
+    int res = i % 2;
+    numDouble = res + numDouble;
+    i = i / 2;
+
+
+}
+Console.Write(numDouble);
+
+
+
+//Console.WriteLine(string.Join(ChangeToDouble));
+// ChangeToDouble(a);
+
