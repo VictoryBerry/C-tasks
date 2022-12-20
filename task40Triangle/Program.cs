09ad6eb7d40003de7fc702fa7681fc107af00c28
Victoria <vikochka90@mail.ru>
2022-12-21 00:06:29 +0300

    Triangle possible

---
+++ task40Triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+
+TrianglePoss(a, b, c);
+
+void TrianglePoss(int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < a + b)
+    {
+        Console.Write($"Triangle possible");
+
+    }
+    else
+    {
+        Console.Write($"Triangle not possible");
+    }
+
+}
+
