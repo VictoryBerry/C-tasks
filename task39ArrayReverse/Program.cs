eb20b1104d2964808f3a421ec69effe53d8236a9
Victoria <vikochka90@mail.ru>
2022-12-20 13:36:49 +0300

    переворот массива

---
+++ task39ArrayReverse/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+// void ReverseArray1(int[] inArray)
+// {
+//     for (int i = 0; i < inArray.Length / 2; i++)
+//     {
+//         int k = inArray[i];
+//         inArray[i] = inArray[inArray.Length - 1 - i];
+//         inArray[inArray.Length - 1 - i] = k;
+//     }
+// }
+
+int[] ReverseArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
+
+int[] array = GetArray(10, 0, 10);
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine();
+int[] reverseArray = ReverseArray2(array);
+//ReverseArray(array);
+Console.WriteLine(string.Join(" ", array));
