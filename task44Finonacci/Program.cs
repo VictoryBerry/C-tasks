f439315f3636d7c6ef8ca5bf3dbb8fb29710b319
Victoria <vikochka90@mail.ru>
2022-12-21 01:58:35 +0300

    Фибоначчи

---
+++ task44Finonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int[] listFib = new int[N];
+listFib[0] = 0; Console.Write(listFib[0]);
+listFib[1] = 1; Console.Write(listFib[1]);
+for (int i = 2; i < N; i++)
+{
+    listFib[i] = listFib[i - 1] + listFib[i - 2];
+    Console.Write(listFib[i]);
+
+}
+
