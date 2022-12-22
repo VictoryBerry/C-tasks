b77231625bdf9089f75b3bdc1f5118dc453162ae
Victoria <vikochka90@mail.ru>
2022-12-23 00:09:30 +0300

    сумма по диагонали

---
+++ task51/Program.cs
@@ -0,0 +1,56 @@
+﻿int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumEl(int[,] array)
+{
+    int sumEl = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j) sumEl = sumEl + array[i, j];
+
+        }
+
+
+    }
+    Console.Write(sumEl);
+}
+
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int colums = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, colums, 0, 10);
+PrintArray(array);
+Console.Write("Сумму элементов по диагонали ");
+SumEl(array);
+
+
