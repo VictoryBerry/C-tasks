c6de6abd7d3e7f77df621cefff21a701055f4d60
Victoria <vikochka90@mail.ru>
2022-12-23 01:02:15 +0300

    вещественные числа

---
+++ Hometask47/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int colums = int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(rows, colums, -10, 10);
+PrintArray(array);
