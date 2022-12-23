449e6a8e9287025a0cf39c8a5cf738e91950c84e
Victoria <vikochka90@mail.ru>
2022-12-23 13:53:59 +0300

    Average

---
+++ Hometask52/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetAverage(int[,] array)
+{
+
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+
+        }
+        Console.WriteLine();
+        double average = sum / array.GetLength(0);
+        Console.Write($"Average --> {average} ");
+        Console.WriteLine();
+
+    }
+
+
+
+}
+
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int colums = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, colums, 0, 10);
+PrintArray(array);
+GetAverage(array);
