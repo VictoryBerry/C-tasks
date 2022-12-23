a2c7c5aa03982d456ff2b0652efde680b4f12860
Victoria <vikochka90@mail.ru>
2022-12-23 17:48:55 +0300

    corrections

--- Hometask47/Program.cs
+++ Hometask47/Program.cs
@@ -1,13 +1,21 @@
-﻿
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-double[,] GetArray(int m, int n, int minValue, int maxValue)
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] GetArray(int m, int n)
 {
     double[,] result = new double[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(minValue, maxValue);
+            result[i, j] = new Random().NextDouble() * 5;
         }
     }
     return result;
@@ -31,5 +39,5 @@ int rows = int.Parse(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 int colums = int.Parse(Console.ReadLine());
 
-double[,] array = GetArray(rows, colums, -10, 10);
+double[,] array = GetArray(rows, colums);
 PrintArray(array);
--- Hometask52/Program.cs
+++ Hometask52/Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -31,7 +37,7 @@ void GetAverage(int[,] array)
 
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        int sum = 0;
+        double sum = 0;
 
         for (int i = 0; i < array.GetLength(0); i++)
         {
