9afaf0fc61f61294fa0d3cedd10be2c001f601f4
Victoria <vikochka90@mail.ru>
2022-12-28 00:20:21 +0300

    Посчитать количество элементов

---
+++ task57/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент
+// входных данных.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FindEl(int[,] array)
+{
+    Console.Write("Введите значение элемента массива N: ");
+    int N = int.Parse(Console.ReadLine());
+    int count = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (N == array[i, j])
+            {
+                count++;
+            }
+
+        }
+        Console.WriteLine();
+    }
+    Console.Write($"Элемент {N} встречается {count} раз");
+}
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int colums = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, colums, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+FindEl(array);
