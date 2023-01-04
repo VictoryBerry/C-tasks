e45b2d64ffe3fbbd2a7787e0b20130ccfbb3fb7d
Victoria <vikochka90@mail.ru>
2023-01-04 23:38:02 +0300

    рекурсия

---
+++ task63/Program.cs
@@ -0,0 +1,13 @@
+﻿// Рекурсия 
+// вывести все натуральные числа от 1 до N
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+void LineNumbers(int N)
+{
+    if (N != 1) LineNumbers(N - 1);
+    Console.Write($" {N} ");
+}
+
+LineNumbers(N);
