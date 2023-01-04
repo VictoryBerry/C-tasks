4d75ea2715dcc38db12364b9b25f4c6670c5f7e9
Victoria <vikochka90@mail.ru>
2023-01-05 00:11:14 +0300

    числа промежутка

---
+++ task65/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от N до M.
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+
+void LineNumbers(int N, int M)
+{
+    if (M > N) LineNumbers(N + 1, M);
+    Console.Write($"{N} ");
+    if (N > M) LineNumbers(N - 1, M);
+
+
+}
+
+LineNumbers(N, M);
