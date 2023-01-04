e7a1d7122a564490e212da193c5e51d60c82a400
Victoria <vikochka90@mail.ru>
2023-01-05 01:18:01 +0300

    Сумма элементов промежутка

---
+++ Hometask66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+
+int LineNumbers(int N, int M)
+{
+    if (M == N) return N;
+
+
+    else return N + LineNumbers(N - 1, M);
+
+
+}
+
+Console.WriteLine(LineNumbers(N, M));
